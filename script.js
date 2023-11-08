a34e0afcd0468e9bea78c191b358e5dd3258cc2a
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-11-08 16:28:41 -0600

    Add buildTree() and prepArray()

--- script.js
+++ script.js
@@ -9,15 +9,35 @@ const node = (data, right, left) => {
 
 // Factory for tree, accepts array when initialized
 const tree = (array) => {
+  // Sort and remove duplicates from array
+  let newArray = prepArray(array);
+
   return {
-    root: buildTree(array),
+    root: buildTree(newArray, 0, newArray.length),
   };
 };
 
 // Function for building tree
-function buildTree(array) {
-  prepArray(array);
-  return 'works';
+function buildTree(array, start, end) {
+  // Exit statement
+  if (start > end) {
+    return null;
+  }
+
+  // Find middle of array
+  let mid = Math.floor((start + end) / 2);
+
+  // Middle of array is current node
+  let currentNode = node(array[mid]);
+
+  // Recursively find left of node
+  currentNode.leftChildren = buildTree(array, start, mid - 1);
+
+  // Recursively find right of node
+  currentNode.rightChildren = buildTree(array, mid + 1, end);
+
+  // Return node
+  return currentNode;
 }
 
 // Function to sort and remove duplicates
@@ -26,11 +46,12 @@ function prepArray(array) {
   array.sort(function (a, b) {
     return a - b;
   });
-  console.log(array);
 
   // Remove any duplicates
   array = array.filter((value, index) => array.indexOf(value) === index);
-  console.log(array);
+
+  // Return array sorted w/ duplicates removed
+  return array;
 }
 
 let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
