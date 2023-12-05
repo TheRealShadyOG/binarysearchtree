d96301f78291744ac3e9c47b0eba42d2024f53c3
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-12-05 11:28:19 -0600

    Add prettyPrint()

--- script.js
+++ script.js
@@ -55,3 +55,18 @@ function prepArray(array) {
 }
 
 let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+// Function to display BST in console
+// Provided by online course I did not write this
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
